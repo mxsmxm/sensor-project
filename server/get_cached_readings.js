3ae24164d51060dbc403dda6f8436b56d5bd4169
BuildTools <unconfigured@null.spigotmc.org>
2018-04-03 14:12:51 +0800

    增加缓存

---
+++ server/get_cached_readings.js
@@ -0,0 +1,24 @@
+const get_sensor_readings=require(./get-readings);
+const cached_readings={temperature:null,humidity:null};
+//dht11数字温湿度传感器的采样周期要大于等于2秒
+setInterval(()=>{
+	get_sensor_readings((err,temperature,huimidity)=>{
+		if(err)
+		{
+			return console.error(err);
+		}
+		else//没有错误便将温湿度保存在缓存对象cached_readings中
+		{
+			cached_readings.temperature=temperature;
+			cached_readings.humidity=huimidity;
+		}
+	});
+},2000);
+
+//将温湿度导出
+module.exports.get_temperature=()=>{
+	cached_readings.temperature;
+};
+module.exports.get_humidity=()=>{
+	cached_readings.humidity;
+};
--- server/index.js
+++ server/index.js
@@ -1,26 +1,15 @@
 const express = require("express");
-const get_sensor_readings=require("./get-reading");
+const get_cached_readings = require("./get_cached_readings");
 const app = express();
 app.get("/temperature", (req, res) => {
-	get_sensor_readings((err,temperature,humidity)=>{
-		if(!err)
-		{
-			let my_temperature=temperature.toFixed(1);
-			console.log(my_temperature);
-			res.send(`${my_temperature}°C`);
-		}
-	});
-
+	let my_temperature = get_cached_readings.get_temperature().toFixed(1);
+	console.log(my_temperature);
+	res.send(`${my_temperature}°C`);
 });
 app.get("/humidity", (req, res) => {
-	get_sensor_readings((err,temperature,humidity)=>{
-		if(!err)
-		{
-			let my_humidity=humidity.toFixed(1);
-			console.log(my_humidity);
-			res.send(`${my_humidity}°C`);
-		}
-	});
+	let my_humidity = get_cached_readings.get_humidity().toFixed(1);
+	console.log(my_humidity);
+	res.send(`${my_humidity}°C`);
 });
 app.listen(3000, () => {
 	console.log("Server listening on port 3000");
