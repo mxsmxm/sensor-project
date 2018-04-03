cecf3e61f2c4523f55f61c63611d16d96f07d865
BuildTools <unconfigured@null.spigotmc.org>
2018-04-03 17:58:04 +0800

    添加静态路由

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const get_cached_readings = require("./get_cached_readings");
 const light_control=require("./light-control");
+const path=require("path");
 const app = express();
+app.use("/public",express.static(path.join(__dirname,"public")));
 app.get("/temperature", function(req, res){
 	let my_temperature = get_cached_readings.get_temperature();
 	console.log(my_temperature);
