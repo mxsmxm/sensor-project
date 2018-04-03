832d0c92ee6b2ad958277c14614fcd77eaca5459
BuildTools <unconfigured@null.spigotmc.org>
2018-04-03 10:20:47 +0800

    dht node server

--- server/index.js
+++ server/index.js
@@ -1,23 +1,21 @@
 const express = require("express");
-const sensor =require("node-dht-sensor");
+const sensor = require("node-dht-sensor");
 const app = express();
 app.get("/temperature", (req, res) => {
-	sensor.read(11,17,function(err,temperature,humidity){
-		if(!err)
-		{
-			let dht_temperature=temperature.toFixed(1);
+	sensor.read(11, 17, function(err, temperature, humidity) {
+		if(!err) {
+			let dht_temperature = temperature.toFixed(1);
 			res.send("${dht_temperature}°C");
-			}
-	res.send("24°C");
+		}
+	});
 });
 app.get("/humidity", (req, res) => {
-	sensor.read(11,17,function(err,temperature,humidity){
-		if(!err)
-		{
-			let dht_humidity=temperature.toFixed(1);
+	sensor.read(11, 17, function(err, temperature, humidity) {
+		if(!err) {
+			let dht_humidity = temperature.toFixed(1);
 			res.send("${dht_humidity}%");
-			}
-	res.send("24°C");
+		}
+	});
 });
 app.listen(3000, () => {
 	console.log("Server listening on port 3000");
