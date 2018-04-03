edd3cc7a4178f49c3b7e032c85da8faa139abe6e
BuildTools <unconfigured@null.spigotmc.org>
2018-04-03 15:33:06 +0800

    增加缓存

--- server/get_cached_readings.js
+++ server/get_cached_readings.js
@@ -7,16 +7,15 @@ setInterval(()=>{
 		{
 			return console.error(err);
 		}
-		 //没有错误便将温湿度保存在缓存对象cached_readings中			
+		 //没有错误便将温湿度保存在缓存对象cached_readings中	
+		 else
+		 {
 			cached_readings.temperature=temperature.toFixed(1);
-			cached_readings.humidity=humidity.toFixed(1);			
+			cached_readings.humidity=humidity.toFixed(1);
+			}
 	});
 },2000);
 
 //将温湿度导出
-module.exports.get_temperature=()=>{
-	return cached_readings.temperature;
-};
-module.exports.get_humidity=()=>{
-	return cached_readings.humidity;
-};
+module.exports.get_temperature=()=>cached_readings.temperature;
+module.exports.get_humidity=()=>cached_readings.humidity;
