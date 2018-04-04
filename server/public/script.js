841fde3f2428db1cdcd9c54c2529b1f27065faf3
BuildTools <unconfigured@null.spigotmc.org>
2018-04-04 19:07:12 +0800

    chart.js

--- server/public/script.js
+++ server/public/script.js
@@ -36,4 +36,22 @@ $(document).ready(function() {
 		fetch_humidity();
 		fetch_temperature();
 	}, 2000)
+	//chart.js 图表
+	const temperatureCanvasCtx = $("#temperature_chart")[0].getContext("2d");
+	const temperatureChart = new Chart(temperatureCanvasCtx, {
+		//将温度信息显示为现状图
+		type: "line",
+		data: {
+			labels: ["8:10", "8:11", "8:12", "8:13", "8:14", "8:15", "8:16", "8:17", "8:18", "8:19"],
+			datasets: [{
+				data: [13, 12, 10, 17, 20, 18, 14, 16, 11, 13],
+				backgroundColor: "rgba(0,200,100,0.5)"
+			}]
+		},
+		options:{
+			legend:{display:false},
+			responsive:true,
+			maintainAspectRatio:false
+		}
+	});
 });
