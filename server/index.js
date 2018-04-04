3d160a581e9862d5c7804beae8e4791d1cf8f0ef
BuildTools <unconfigured@null.spigotmc.org>
2018-04-04 19:55:04 +0800

    json

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ app.use("/public",express.static(path.join(__dirname,"public")));
 app.get("/temperature", function(req, res){
 	let my_temperature = get_cached_readings.get_temperature();
 	console.log(my_temperature);
-	res.send(`${my_temperature}°C`);
+	res.json({val:my_temperature});
 });
 app.get("/humidity", function(req, res){
 	let my_humidity = get_cached_readings.get_humidity();
 	console.log(my_humidity);
-	res.send(`${my_humidity}%`);
+	res.json({val:my_humidity}});
 });
 app.get("/on", function(req, res){
 	light_control.on();
